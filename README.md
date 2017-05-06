85ee6fa785e95fae144e8da31c440498d9d75daa
litong <921006756@qq.com>
2017-05-06 18:52:22 +0800

    提交初始版本
    Signed-off-by: litong <921006756@qq.com>

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # imgWaterfall
+
+
+##### 使用方法：
+
+引入jquery，及waterfall
+```
+<script src='jquery.js'></script>
+<script src='waterfall.js'></script>
+```
+
+##### html结构：
+```
+<div class="box">
+	<img src="src" alt="" class="img">
+	<p class="inf">this is inf</p>
+</div>
+```
+
+##### 使用方法:
+```
+<script>
+	$(function(){
+		$('.box').waterfall({
+			imgWidth:190  //图片宽度
+		})
+	})
+</script>
+```
+
+传入的配置参数目前只有一个，用于配置图片宽度
---
+++ demo/js/waterfall.js
@@ -0,0 +1,51 @@
+/*
+ litong 
+ 瀑布流插件
+ 传入图片宽度
+*/
+(function($) {
+	$.fn.waterfall = function(options){
+		// console.log('adsf')
+		var settings = $.extend({
+				imgWidth:190,
+			},options);
+
+		// 设置图片宽度：
+		var $box = this;
+		$box.find('img').attr('width',settings.imgWidth);
+
+		var boxOuterWidth = $box.eq(0).outerWidth(true);
+		//设置整体居中
+		// console.log('图片宽度加margin'+imgWidth)
+		
+		var boxArr = []; // 存放第一行图片的所有高度。
+		var colNum =  Math.floor(($(window).width())/boxOuterWidth);  // 计算可以放几列图片；
+		console.log("可以放"+colNum+"列图片")
+
+		this.each(function(index,value) {
+			var $this = $(this);
+			if(index<colNum){
+				boxArr.push($this.outerHeight(true));
+			}else{
+				// 获取boxArr
+				var minHeight = Math.min.apply(null,boxArr);
+				// console.log(minHeight);
+				var minIndex = boxArr.indexOf(minHeight);
+				// console.log(minIndex)
+				// console.log($box[minIndex])
+				var left = $box.eq(minIndex).position().left;
+				// console.log(left)
+				var addHeight = $box.eq(index).css({
+					"position":'absolute',
+					"left":left,
+					"top":minHeight
+				}).outerHeight(true);
+
+				boxArr[minIndex] += addHeight;
+			}
+			
+		})
+			console.log(boxArr)
+	
+	}
+})(jQuery)
